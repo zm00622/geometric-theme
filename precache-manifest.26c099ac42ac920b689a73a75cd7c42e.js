703b27579619b15b22d36e6bfbbb1674cab22e7b
zm00622 <zarymanning@gmail.com>
2020-02-18 12:27:04 -0500

    Updates

---
+++ precache-manifest.26c099ac42ac920b689a73a75cd7c42e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac97c8441a330b5c6688d0d6a3a05f91",
+    "url": "/geometric-theme/index.html"
+  },
+  {
+    "revision": "c73f1bd84d26bda87363",
+    "url": "/geometric-theme/static/css/main.0d23c03a.chunk.css"
+  },
+  {
+    "revision": "d8c4eb224596735c6f36",
+    "url": "/geometric-theme/static/js/2.c41d7a3e.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/geometric-theme/static/js/2.c41d7a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73f1bd84d26bda87363",
+    "url": "/geometric-theme/static/js/main.ddcb5258.chunk.js"
+  },
+  {
+    "revision": "588068d314bedfa94acc",
+    "url": "/geometric-theme/static/js/runtime-main.c5222046.js"
+  },
+  {
+    "revision": "188fbd6a6114b14d8aba002cfcc6da74",
+    "url": "/geometric-theme/static/media/MyPic.188fbd6a.jpg"
+  },
+  {
+    "revision": "37b3ad32295cada45f53b450425a7f6b",
+    "url": "/geometric-theme/static/media/small-geometric.37b3ad32.png"
+  }
+]);
